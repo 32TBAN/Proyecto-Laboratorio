b3aa90095ad4ea42bfc5a8101f47484ba377492b
32TBAN <estebanguanoluisa55@gmail.com>
2023-07-14 23:58:43 -0500

    version 0.0.7

--- client/src/main.js
+++ client/src/main.js
@@ -1,20 +1,49 @@
-import { createApp } from "vue";
+/*!
+
+ =========================================================
+ * Vue Light Bootstrap Dashboard - v2.1.0 (Bootstrap 4)
+ =========================================================
+
+ * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
+ * Copyright 2023 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 
-import { createRouter, createWebHistory } from "vue-router";
-import Inicio from "./components/Inicio.vue";
-import RegistroPacientes from "./components/RegistroPaciente.vue";
-import Buscar from "./components/ListaPacientes.vue";
-import Laboratorio from "./components/ListaExamenes.vue";
+// LightBootstrap plugin
+import LightBootstrap from "./light-bootstrap-main";
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", component: Inicio },
-    { path: "/registro-pacientes", component: RegistroPacientes },
-    { path: "/buscar", component: Buscar },
-    { path: "/laboratorio", component: Laboratorio },
-  ],
+// router setup
+import routes from "./routes/routes";
+
+import "./registerServiceWorker";
+// plugin setup
+Vue.use(VueRouter);
+Vue.use(LightBootstrap);
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: "nav-item active",
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
-createApp(App).use(router).mount("#app");
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+  router,
+});
