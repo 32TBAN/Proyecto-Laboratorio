cb6ed0e291bcf828f56f9ff73e4cc113711979ab
32TBAN <estebanguanoluisa55@gmail.com>
2023-07-15 12:26:27 -0500

    version 0.0.8

--- client/src/main.js
+++ client/src/main.js
@@ -1,36 +1,12 @@
-/*!
-
- =========================================================
- * Vue Light Bootstrap Dashboard - v2.1.0 (Bootstrap 4)
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
- * Copyright 2023 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
-import Vue from "vue";
-import VueRouter from "vue-router";
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-
-// LightBootstrap plugin
-import LightBootstrap from "./light-bootstrap-main";
-
-// router setup
 import routes from "./routes/routes";
 
-import "./registerServiceWorker";
-// plugin setup
-Vue.use(VueRouter);
-Vue.use(LightBootstrap);
-
-// configure router
-const router = new VueRouter({
-  routes, // short for routes: routes
+// Usar VueRouter
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
   linkActiveClass: "nav-item active",
   scrollBehavior: (to) => {
     if (to.hash) {
@@ -41,9 +17,11 @@ const router = new VueRouter({
   },
 });
 
-/* eslint-disable no-new */
-new Vue({
-  el: "#app",
-  render: (h) => h(App),
-  router,
-});
+// Crea la instancia de la aplicación
+const app = createApp(App);
+
+// Usa VueRouter
+app.use(router);
+
+// Monta la aplicación en el elemento con el ID "app"
+app.mount("#app");
